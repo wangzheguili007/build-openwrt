a91d7f63d50c2a388bef9b2944c7ac3d0a017fec
lieoxc <xiaocongli45@gmail.com>
2025-03-14 00:09:54 +0800

    ax6s,ax3000T,CR660x,router3都增加webPlugin插件，以及替换logo，默认LAN口IP

--- config/immortalwrt-xiaomi-router3/diy-part2.sh
+++ config/immortalwrt-xiaomi-router3/diy-part2.sh
@@ -16,7 +16,9 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.51.1/g' package/base-files/files/bin/config_generate
+# Modify default HostName（FROM ImmortalWrt CHANGE TO WorldSnake）
+sed -i "s/set system\.@system\[-1\]\.hostname='ImmortalWrt'/set system\.@system\[-1\]\.hostname='WorldSnake'/" package/base-files/files/bin/config_generate
 #
 # ------------------------------- Main source ends -------------------------------
 
@@ -24,6 +26,14 @@ echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_r
 #
 # Add luci-app-amlogic
 # svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+
+git clone https://github.com/Lieoxc/op_web3Plugin.git package/op_web3Plugin
+## remove unsupported arm64
+rm -rf package/op_web3Plugin/webPlugin-arm64
+
+## replace logo
+cp -rf package/op_web3Plugin/argon.svg ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/
+cp -rf package/op_web3Plugin/argon.svg ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/fonts/
 
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
